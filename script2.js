09dccfc46d8047f6be7cbe03d13818cd3a7a5ba3
Aman <amansaxena.4654@gmail.com>
2022-11-05 22:42:54 +0530

    first commit

---
+++ script2.js
@@ -0,0 +1,47 @@
+const context = document.getElementById("data-set").getContext("2d");
+let line = new Chart(context, {});
+//Values from the form
+const intialAmount = document.getElementById("initialamount");
+const years = document.getElementById("years");
+const rates = document.getElementById("rates");
+const compound = document.getElementById("compound");
+
+//Message
+const message = document.getElementById("message");
+
+//The calculate button
+const button = document.querySelector(".input-group button");
+//Attach an event listener
+button.addEventListener("click", calculateGrowth);
+
+const data = [];
+const labels = [];
+
+function calculateGrowth(e) {
+    e.preventDefault();
+    data.length = 0;
+    labels.length = 0;
+    let growth = 0;
+    try {
+        const initial = parseInt(intialAmount.value);
+        const period = parseInt(years.value);
+        const interest = parseInt(rates.value);
+        const comp = parseInt(compound.value);
+
+        for(let i = 1; i <= period; i++) {
+            const final = initial * Math.pow(1 + ((interest / 100) / comp), comp * i);
+            data.push(toDecimal(final, 2));
+            labels.push("Year " + i);
+            growth = toDecimal(final, 2);
+        }
+        //
+        message.innerText = `You will have this amount ${growth} after ${period} years`;
+        drawGraph();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function toDecimal(value, decimals) {
+    return +value.toFixed(decimals);
+}
